fde8ab32f7aa751d81ddf7243de7d876f2592fa3
kartikbitla <103336030+kartikbitla@users.noreply.github.com>
2023-10-18 15:31:51 +0000

    variables studied

---
+++ 01.Basics/01.variables.js
@@ -0,0 +1,12 @@
+const accountId = 13344;
+let accountEmail = "kartikbitla@gmail.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+let accountState;
+
+//accountId = 2;
+accountEmail = "hc@hc.com";
+accountPassword = "qr23434fdfdf";
+accountCity = "Begaluru";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
