584dcdafcf2ef437e2246a597c3ee35d7577a3bd
kartikbitla <103336030+kartikbitla@users.noreply.github.com>
2023-10-19 02:04:19 +0000

    conversion

---
+++ 01.Basics/03.conversion.js
@@ -0,0 +1,14 @@
+let a = "kartik";
+let b = "";
+let c = 0;
+let d =1;
+
+a = Boolean(a);
+b = Boolean(b);
+c = Boolean(c);
+d = Boolean(d);
+console.table([a,b,c,d]);
+
+let someNumber = 33;
+let stringNUmber = String(someNumber);
+console.log(typeof stringNUmber);
