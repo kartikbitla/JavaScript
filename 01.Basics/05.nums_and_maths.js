934d8f027650d3444bc1b60a724ac3efb0abb570
Kartik Bitla <bitlakartik@gmail.com>
2023-10-20 14:58:20 +0000

    practice in maths and number

---
+++ 01.Basics/05.nums_and_maths.js
@@ -0,0 +1,20 @@
+// const score = 199;
+// const number = new Number(100);
+
+// console.log(score);
+// console.log(number);
+
+// console.log(score.toString().length);
+// console.log(number.toFixed(2));
+
+// const otherNumber = 23.866;
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+//========== MATHS =========
+
+console.log(Math);
