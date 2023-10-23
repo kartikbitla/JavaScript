6d210fed6cf17c189fbebeaa4829ca21e0bb0142
Kartik Bitla <bitlakartik@gmail.com>
2023-10-23 14:09:48 +0000

    Dates and Time

---
+++ 01.Basics/06.dates_and_time.js
@@ -0,0 +1,17 @@
+//Dates
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString("en-IN"));
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date(2023, 0, 23); //to mention a fixed date
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getDate());
