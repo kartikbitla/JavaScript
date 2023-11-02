d64ac82e2449af23cbe06e542a450ddfcf0f4362
Kartik Bitla <bitlakartik@gmail.com>
2023-11-02 10:32:27 +0000

    WIP

--- functions/functions.js
+++ functions/functions.js
@@ -18,3 +18,73 @@ function calculateCarPrice(...price){   //three dots before the parameter called
 
 
 console.log(calculateCarPrice(200, 400, 600));
+
+
+
+//TYPES OF FUNCTIONS
+
+
+//function declaration
+console.log(addOne(10));  //you can call it before declaration of this kind of function
+function addOne(num){  
+    return num+1;
+}
+
+
+
+//function expression
+//console.log(addTwo(20));  //we can not call it before the declaration, this will give an error
+const addTwo = function(num){ 
+    return num+2;
+}
+
+
+
+const userDetails = {
+    userName:"Kartik",
+    pric:999,
+
+    welcomeMessage: function(){
+        console.log(`${this.userName}, welcome to website`);
+        console.log(this);
+    }
+}
+
+
+// userDetails.welcomeMessage();
+// userDetails.userName = "sam";
+// userDetails.welcomeMessage();
+
+//console.log(this);
+
+
+function hello(){
+    let userName = "kartik";
+    //console.log(this);
+    console.log(this.userName);
+}
+
+hello();
+
+
+
+
+//arrow function
+const hellos = () => {
+    let userName = "world"; 
+    console.log(this.userName);
+}
+
+hellos();
+
+
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(sum(1,5));
+
+
+const sums = (num1, num2) => num1 + num2; //this is called implicit return
+
+console.log(sums(10,5));
---
+++ scopes/scopes.js
@@ -0,0 +1,19 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+
+if(true){
+    let a = 20;
+    const b = 40;
+    var c = 60;
+
+    console.log("innner " + a);
+    console.log("inner " + b);
+    console.log("inner " + c);
+}
+
+
+console.log(a);
+console.log(b);
+console.log(c);
